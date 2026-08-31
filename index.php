06b385b5272549a8e2ee40d90394c19c15c05f89
Asadel Hugo <hueyswork@gmail.com>
2026-08-31 14:46:42 +0700

    Add files via upload
    Raden Mas Asadel Hugo Syandana - XI PPLG 2

---
+++ index.php
@@ -0,0 +1,23 @@
+<?php
+
+$nama = "Hugo";
+$kelas = "XI PPLG 2";
+$bilSatu = 2;
+$bilDua = 5;
+$hitung = $bilSatu + $bilDua;
+
+echo "Halo, nama saya ";
+echo $nama;
+echo "<br/> Saya berasal dari kelas ";
+echo $kelas;
+echo "<br/> Kali ini, saya akan membuat perhitungan sederhana. <br/> <br/>";
+
+echo "Bilangan satu = ";
+echo $bilSatu;
+echo "<br/> Bilangan dua = ";
+echo $bilDua;
+echo "<br/>";
+echo "Hasil pertambahan kedua bilangan = ";
+echo $hitung;
+
+?>

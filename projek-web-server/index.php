3dd3265cbddb4a6d5ff5b2a7b0090e0b831e4ced
rakhaparkerr <jajawajawajawajaw@gmail.com>
2026-08-31 14:46:54 +0700

    Add files via upload
    andi ralha arrayyan-xi pplg 2

---
+++ projek-web-server/index.php
@@ -0,0 +1,13 @@
+<?php
+
+$namakelas = "XI PPLG-2";
+$bilanganSatu = 1;
+$bilanganDua = 3;
+$hitung = $bilanganSatu + $bilanganDua;
+echo "halo orang!";
+echo "<br/>";
+echo $namakelas;
+echo "<br/>";
+echo $hitung;
+
+?>

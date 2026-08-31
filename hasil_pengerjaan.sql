72f834d4c1087b77460da87819e8f63d7391a9f0
Asadel Hugo <hueyswork@gmail.com>
2026-08-31 13:00:52 +0700

    Add files via upload
    Raden Mas Asadel Hugo Syandana - XI PPLG 2

---
+++ hasil_pengerjaan.sql
@@ -0,0 +1,79 @@
+-- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
+--
+-- Host: localhost    Database: kelas_pplg_db
+-- ------------------------------------------------------
+-- Server version	10.4.32-MariaDB
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `biodata_siswa`
+--
+
+DROP TABLE IF EXISTS `biodata_siswa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `biodata_siswa` (
+  `id` int(20) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `tempat_tgl_lahir` varchar(15) NOT NULL,
+  `jenis_kelamin` enum('Laki-laki','Perempuan') NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `biodata_siswa`
+--
+
+LOCK TABLES `biodata_siswa` WRITE;
+/*!40000 ALTER TABLE `biodata_siswa` DISABLE KEYS */;
+INSERT INTO `biodata_siswa` VALUES (0,'Asadel Hugo','Jakarta, 07-02-','Laki-laki','Jl. Notaris 004/01 62A');
+/*!40000 ALTER TABLE `biodata_siswa` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `siswa`
+--
+
+DROP TABLE IF EXISTS `siswa`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `siswa` (
+  `NIS` int(10) NOT NULL,
+  `nama_siswa` varchar(50) NOT NULL,
+  `kelas` varchar(20) DEFAULT NULL,
+  PRIMARY KEY (`NIS`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `siswa`
+--
+
+LOCK TABLES `siswa` WRITE;
+/*!40000 ALTER TABLE `siswa` DISABLE KEYS */;
+INSERT INTO `siswa` VALUES (1,'Ahnaf Rakha Rizqullah','XI PPLG 1'),(2,'Andi Rakha Arrayan','XI PPLG 2'),(3,'Arfah Putra Subandi','XI PPLG 3'),(4,'Fajar Raihan Ash-Shidiq','XI PPLG 3'),(5,'Irzi Mubarok','XI PPLG 1'),(6,'Muhammad Rizqi Fadillah','XI PPLG 1'),(7,'Pandaya Batara Galih','XI PPLG 2'),(8,'Raden Mas Asadel Hugo','XI PPLG 2'),(9,'Reza Misbach','XI PPLG 3'),(10,'Rifqi Arrafif Hakim','XI PPLG 3'),(11,'Sayyidina Alli','XI PPLG 3'),(12,'Zidane Ferdian Desta Andika','XI PPLG 1');
+/*!40000 ALTER TABLE `siswa` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2026-08-31 12:49:45

25c728706501a0256ff6fbeaa4a0ca8f6e442a2c
Maureen Alya <maureencans08@gmail.com>
2026-08-31 21:33:17 +0700

    databse punya maureennnnnnnn

--- hasil_pengerjaan.sql
+++ hasil_pengerjaan.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 8.4.3, for Win64 (x86_64)
+-- MySQL dump 10.13  Distrib 8.0.30, for Win64 (x86_64)
 --
--- Host: localhost    Database: bd_siswa
+-- Host: localhost    Database: biodata_siswa_db
 -- ------------------------------------------------------
--- Server version	8.4.3
+-- Server version	8.0.30
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,30 +16,30 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
--- Table structure for table `biodata_siswa`
+-- Table structure for table `tb_biodata`
 --
 
-DROP TABLE IF EXISTS `biodata_siswa`;
+DROP TABLE IF EXISTS `tb_biodata`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!50503 SET character_set_client = utf8mb4 */;
-CREATE TABLE `biodata_siswa` (
+CREATE TABLE `tb_biodata` (
   `ID` varchar(20) NOT NULL,
   `Nama` varchar(30) NOT NULL,
-  `TTL` varchar(50) NOT NULL,
-  `jenis_kelamin` varchar(10) NOT NULL,
+  `Tempat_tanggal_lahir` varchar(15) NOT NULL,
+  `Jenis_kelamin` varchar(10) NOT NULL,
   `Alamat` varchar(50) NOT NULL,
   PRIMARY KEY (`ID`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `biodata_siswa`
+-- Dumping data for table `tb_biodata`
 --
 
-LOCK TABLES `biodata_siswa` WRITE;
-/*!40000 ALTER TABLE `biodata_siswa` DISABLE KEYS */;
-INSERT INTO `biodata_siswa` VALUES ('10203','panjul','Jakarta-17-08-2001','Pria','Banten'),('20304','dzulfikar','Jakarta-22-08-2099','Pria','NewYork'),('30405','Wisuda','Tokyo-11-01-2111','Pria','Shibuya');
-/*!40000 ALTER TABLE `biodata_siswa` ENABLE KEYS */;
+LOCK TABLES `tb_biodata` WRITE;
+/*!40000 ALTER TABLE `tb_biodata` DISABLE KEYS */;
+INSERT INTO `tb_biodata` VALUES ('1','Maureen','Jakarta','Perempuan','Jl. Anggrek No 9'),('2','Silvi','Jakarta','Perempuan','Jl. Mawar No 8'),('3','Ibrahim','Jakarta','laki-laki','Jl. Kenangan No 7'),('4','Fadil','Tangerang','laki-laki','Jl. Taman Asri No 86'),('5','Ziedane','Jakarta','laki-laki','Jl. Melati No 90');
+/*!40000 ALTER TABLE `tb_biodata` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -51,4 +51,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-08-31 13:43:20
+-- Dump completed on 2026-08-31 13:01:49

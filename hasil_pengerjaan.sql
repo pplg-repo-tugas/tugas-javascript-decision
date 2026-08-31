80bc1494417e8291eefe31111145e3c927d42a13
Pandaya Batara Galih <lihhzyv2@gmail.com>
2026-08-31 13:13:54 +0700

    Add files via upload
    Pandaya Batara Galih XI PPLG 2

--- hasil_pengerjaan.sql
+++ hasil_pengerjaan.sql
@@ -1,6 +1,6 @@
 -- MariaDB dump 10.19  Distrib 10.4.32-MariaDB, for Win64 (AMD64)
 --
--- Host: localhost    Database: kelas_pplg_db
+-- Host: localhost    Database: pandaya_kelas_pplg2
 -- ------------------------------------------------------
 -- Server version	10.4.32-MariaDB
 
@@ -23,12 +23,12 @@ DROP TABLE IF EXISTS `biodata_siswa`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `biodata_siswa` (
-  `kolom_id` varchar(20) NOT NULL,
-  `Nama` varchar(30) NOT NULL,
-  `Tempat_tanggal_lahir` varchar(15) NOT NULL,
-  `Jenis_kelamin` varchar(10) NOT NULL,
-  `Alamat` varchar(50) NOT NULL,
-  PRIMARY KEY (`kolom_id`)
+  `id` int(20) NOT NULL,
+  `nama` varchar(30) NOT NULL,
+  `tempat_tgl_lahir` varchar(15) NOT NULL,
+  `jenis_kelamin` enum('Laki-laki','Perempuan') NOT NULL,
+  `alamat` varchar(50) NOT NULL,
+  PRIMARY KEY (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_general_ci;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
@@ -38,19 +38,19 @@ CREATE TABLE `biodata_siswa` (
 
 LOCK TABLES `biodata_siswa` WRITE;
 /*!40000 ALTER TABLE `biodata_siswa` DISABLE KEYS */;
-INSERT INTO `biodata_siswa` VALUES ('51535','rakha','jakarta','laki-laki','jl hajigari');
+INSERT INTO `biodata_siswa` VALUES (0,'Pandaya Cumsky','Jakarta, 07-02-','Laki-laki','Jl. Notaris 004/01 62A');
 /*!40000 ALTER TABLE `biodata_siswa` ENABLE KEYS */;
 UNLOCK TABLES;
 
 --
--- Table structure for table `tb_siswa`
+-- Table structure for table `siswa`
 --
 
-DROP TABLE IF EXISTS `tb_siswa`;
+DROP TABLE IF EXISTS `siswa`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `tb_siswa` (
-  `NIS` varchar(10) NOT NULL,
+CREATE TABLE `siswa` (
+  `NIS` int(10) NOT NULL,
   `nama_siswa` varchar(50) NOT NULL,
   `kelas` varchar(20) DEFAULT NULL,
   PRIMARY KEY (`NIS`)
@@ -58,13 +58,13 @@ CREATE TABLE `tb_siswa` (
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
--- Dumping data for table `tb_siswa`
+-- Dumping data for table `siswa`
 --
 
-LOCK TABLES `tb_siswa` WRITE;
-/*!40000 ALTER TABLE `tb_siswa` DISABLE KEYS */;
-INSERT INTO `tb_siswa` VALUES ('1245','rakha','11 pplg');
-/*!40000 ALTER TABLE `tb_siswa` ENABLE KEYS */;
+LOCK TABLES `siswa` WRITE;
+/*!40000 ALTER TABLE `siswa` DISABLE KEYS */;
+INSERT INTO `siswa` VALUES (1,'Ahnaf Rakha Rizqullah','XI PPLG 1'),(2,'Andi Rakha Arrayan','XI PPLG 2'),(3,'Arfah Putra Subandi','XI PPLG 3'),(4,'Fajar Raihan Ash-Shidiq','XI PPLG 3'),(5,'Irzi Mubarok','XI PPLG 1'),(6,'Muhammad Rizqi Fadillah','XI PPLG 1'),(7,'Pandaya Batara Galih','XI PPLG 2'),(8,'Raden Mas Asadel Hugo','XI PPLG 2'),(9,'Reza Misbach','XI PPLG 3'),(10,'Rifqi Arrafif Hakim','XI PPLG 3'),(11,'Sayyidina Alli','XI PPLG 3'),(12,'Zidane Ferdian Desta Andika','XI PPLG 1');
+/*!40000 ALTER TABLE `siswa` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -76,4 +76,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2026-08-31 12:54:45
+-- Dump completed on 2026-08-31 12:49:45
